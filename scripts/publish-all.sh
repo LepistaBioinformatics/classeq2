f5199c2b59fefe39f74219eb0be20df19c9ad4fc
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-07-07 17:05:30 -0300

    build: include new packages to the publish-all scriot
    main

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -3,7 +3,7 @@
 ARGS=("$@")
 
 cargo publish -p classeq-core $ARGS
-cargo publish -p classeq-cli $ARGS
 cargo publish -p classeq-ports-lib $ARGS
+cargo publish -p classeq-cli $ARGS
 cargo publish -p classeq-api $ARGS
 cargo publish -p classeq-watcher $ARGS
