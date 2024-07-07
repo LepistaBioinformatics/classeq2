92e9347b8d911e1013002eac2fb2b65b7f5907c9
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-07-07 17:00:07 -0300

    build: fix tracing-subscriber version
    main

--- scripts/publish-all.sh
+++ scripts/publish-all.sh
@@ -4,3 +4,6 @@ ARGS=("$@")
 
 cargo publish -p classeq-core $ARGS
 cargo publish -p classeq-cli $ARGS
+cargo publish -p classeq-ports-lib $ARGS
+cargo publish -p classeq-api $ARGS
+cargo publish -p classeq-watcher $ARGS
