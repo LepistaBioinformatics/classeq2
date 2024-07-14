7ef09bf7356789a6bb87d88789df43675cc66e0c
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-07-14 10:25:24 -0300

    build: upgrade build elements before docker images publishing
    main

--- ports/lib/src/models/models_config.rs
+++ ports/lib/src/models/models_config.rs
@@ -5,12 +5,27 @@ use uuid::Uuid;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ModelConfig {
+    /// The model ID
+    ///
+    /// This is the same ID generated during the model building using the CLI
+    /// tool.
     pub id: Uuid,
+
+    /// The model name
+    ///
+    /// An arbitrary name given to the model a human-readable name.
     pub name: String,
 
+    /// The gene name
+    ///
+    /// The gene name is the name of the gene used to build the phylogeny/model.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub gene: Option<String>,
 
+    /// Path to the model file
+    ///
+    /// The serialization are skipped to avoid exposing the path during the API
+    /// client responses.
     #[serde(skip_serializing)]
     path: PathBuf,
 }
