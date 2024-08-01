62325aba0985b5cd02264e00d906de69d271f035
Samuel Galvão Elias <samuel.elias@biotrop.com.br>
2024-08-01 08:34:08 -0300

    refactor: rename the map-kmers-to-tree to build-database to preciselly indicate their goal
    main

--- core/src/use_cases/mod.rs
+++ core/src/use_cases/mod.rs
@@ -1,6 +1,6 @@
 /// This module contains the use case to map kmers from a multiple sequences
 /// fasta file to a phylogenetic tree.
-mod map_kmers_to_tree;
+mod build_database;
 
 /// This module contains the use case to place sequences on a model generated
 /// from a phylogenetic tree.
@@ -9,5 +9,5 @@ mod place_sequences;
 /// Elements of shared module are restricted to be used only in this crate.
 mod shared;
 
-pub use map_kmers_to_tree::*;
+pub use build_database::*;
 pub use place_sequences::*;
